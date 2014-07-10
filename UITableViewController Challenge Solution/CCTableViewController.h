c6c3b107cb7c3f6142184a153020c5eb35012334
Che' Bustos <chebustos@Ches-MacBook.local>
2014-07-10 15:19:00 -0600

    solve thechallenge

--- UITableViewController Challenge Solution/CCTableViewController.h
+++ UITableViewController Challenge Solution/CCTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  CCViewController.h
+//  CCTableViewController.h
 //  UITableViewController Challenge Solution
 //
 //  Created by Che' Bustos on 7/10/14.
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface CCViewController : UIViewController
+@interface CCTableViewController : UITableViewController
 
 @end
